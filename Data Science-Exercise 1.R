e604f161aefc3be5d0391143ffa39489a2d73432
christopherdiuyan <christopher.diuyan@gmail.com>
2020-12-14 18:48:41 +0800

    test1

--- Data Science-Exercise 1.R
+++ Data Science-Exercise 1.R
@@ -1,8 +1,4 @@
 library(dplyr)
 college_df <- read.csv(file.choose(), header = T)
-<<<<<<< HEAD
-shamenedfvds
-=======
-sasasa
->>>>>>> 580456223135651f6e18027e4f0870069f5f1108
-akoJC123
+
+print(paste("The number of entries are: ", nrow(college_df)))
